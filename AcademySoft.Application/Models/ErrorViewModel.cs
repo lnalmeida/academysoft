959777f3df203766324348c0a358d2bdc546f469
Eduardo.Lopes <eduardo.lopes-ext@telecall.com>
2023-10-08 13:05:50 -0300

    Created first migration and entities.

---
+++ AcademySoft.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AcademySoft.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- AcademySoft.Infra.Data/Context/DataContext.cs
+++ AcademySoft.Infra.Data/Context/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AcademySoft.Domain.Entities.Account;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,9 @@ using System.Threading.Tasks;
 
 namespace AcademySoft.Infra.Data.Context
 {
-    internal class DataContext : DbContext
+    public class DataContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
         public DataContext(DbContextOptions<DataContext> options): base(options){}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
